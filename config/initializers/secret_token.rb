50b87e5d2b5140de7bcecb047d7c5bdbef42a658
root <Joel>
2014-04-05 21:26:26 -0300

    Primer revisión del foro

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foro::Application.config.secret_key_base = '0a24cc5f960156df6c025845bf37fee9fab65de7e06f59c17115c695d6a9e50e72e894c45f00c2f5a18c1b10d9806ec2f2d12dfb2d556548107154653e345c34'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foro::Application.config.session_store :cookie_store, key: '_foro_session'
